fa2aab0e9e5153388d680c7af7d18b844dea3ba5
Stefano Salsano <stefano.salsano@uniroma2.it>
2024-07-19 00:24:26 +0200

    Update megatron-setup.sh

--- megatron-setup.sh
+++ megatron-setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-#run this script root user
+#run this script as root user
 
 cd /home/mpi
-su mpi
+
 mkdir megatron
-
-git clone https://github.com/NVIDIA/Megatron-LM.git
-
 mkdir tensorboard_logs
 mkdir data
 mkdir checkpoint
+git clone https://github.com/NVIDIA/Megatron-LM.git
 
+chown -R mpi:mpi /home/mpi
 
